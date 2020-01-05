c4b999ed79a629594d68e0ea219d583f356079e6
Ethan Hasman <hasman1982@gmail.com>
2020-01-05 16:05:14 +0200

    Creating Repo, Testing New Comp. Import Bootstrap

---
+++ src/ActorCard/ActorCard.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Card from 'react-bootstrap/Card';
+
+export default class ActorCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+
+         }
+    }
+    render() { 
+        return (    
+            <div>
+                Testing this
+            </div>
+          );
+    }
+}
+ 
+// export default ActorCard;
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ActorCard from "./ActorCard/ActorCard"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Aloha Coder</h1>
+      <ActorCard/>
+    </div>
+  );
+}
+
+export default App;
