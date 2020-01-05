a5f87ffd55105bb18c2cd697c708c4e60cc0fef9
Ethan Hasman <hasman1982@gmail.com>
2020-01-05 16:13:41 +0200

    Cahanging Components Folder Name

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ActorCard from "./ActorCard/ActorCard"
+import ActorCard from "./Components/ActorCard"
 
 function App() {
   return (
