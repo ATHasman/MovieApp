09ec6edafca6418b82d7f2d9b8825f7b382948b1
Ethan Hasman <hasman1982@gmail.com>
2020-01-06 10:04:36 +0200

    Edits

--- src/Components/ActorCard.jsx
+++ src/Components/ActorCard.jsx
@@ -17,7 +17,7 @@ export default class ActorCard extends Component {
             <Card style={{ width: '18rem' }}>
             <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
             <Card.Body>
-            <Card.Title>{ActorsData[0].firstName}</Card.Title>
+            <Card.Title>{`${ActorsData[0].firstName} ${ActorsData[0].lastName}`}</Card.Title>
                 <Card.Text>
                 Some quick example text to build on the card title and make up the bulk of
                 the card's content.
@@ -30,7 +30,6 @@ export default class ActorCard extends Component {
             </ListGroup>
             <Card.Body>
                 <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
             </Card.Body>
             </Card>
             </div>
