302396cd45dc0e0501b4607d3672f2356ba9198c
Ethan Hasman <hasman1982@gmail.com>
2020-01-07 22:00:59 +0200

    adding Container Comp. to main App.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ActorCard from "./Components/ActorCard"
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
     <div className="App">
-      <ActorCard/>
+      <Container>
+        <ActorCard/>
+      </Container>
     </div>
   );
 }
--- src/Components/ActorCard.jsx
+++ src/Components/ActorCard.jsx
@@ -4,6 +4,10 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import ListGroupItem from 'react-bootstrap/ListGroupItem';
 import ActorsData from '../Data/ActorsData'
 
+
+// AcotrCard Props: 
+//      actorDataIndex (number) - in ActorsData model objArray
+//      
 export default class ActorCard extends Component {
     constructor(props) {
         super(props);
@@ -12,18 +16,18 @@ export default class ActorCard extends Component {
          }
     }
     render() { 
+
         return (   
             <div>
             <Card style={{ width: '18rem' }}>
                 <Card.Img variant="top" src={ActorsData[0].image} />
                 <Card.Body>
-                <Card.Title><Card.Link href={ActorsData[0].imdbLink}>{`${ActorsData[0].firstName} ${ActorsData[0].lastName}`}</Card.Link></Card.Title>
-                <ListGroup className="list-group-flush">
-                    <ListGroupItem>Date of Birth: {ActorsData[0].birthDay}</ListGroupItem>
-                    <ListGroupItem>Age: {ActorsData[0].birthDay}</ListGroupItem>
-                </ListGroup>
+                    <Card.Title><Card.Link href={ActorsData[0].imdbLink}>{`${ActorsData[0].firstName} ${ActorsData[0].lastName}`}</Card.Link></Card.Title>
+                    <ListGroup className="list-group-flush">
+                        <ListGroupItem>Date of Birth: {ActorsData[0].birthDay}</ListGroupItem>
+                        <ListGroupItem>Age: {ActorsData[0].birthDay}</ListGroupItem>
+                    </ListGroup>
                 </Card.Body>
-                
             </Card>
             </div>
           );
