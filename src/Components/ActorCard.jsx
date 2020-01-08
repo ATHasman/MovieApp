0a7615e54fc7ca5336e488d71faab14647661993
Ethan Hasman <hasman1982@gmail.com>
2020-01-08 09:41:47 +0200

    editing ActorCard Comp.

--- src/Components/ActorCard.jsx
+++ src/Components/ActorCard.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import ListGroupItem from 'react-bootstrap/ListGroupItem';
-import ActorsData from '../Data/ActorsData'
+import ActorsData from '../Data/ActorsData.json'
+import actorModel from '../Data/ActorModel.js'
 
 
 // AcotrCard Props: 
@@ -12,20 +13,22 @@ export default class ActorCard extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-
+            key : ""
+            
          }
     }
     render() { 
-
+        const displayActor = new actorModel (ActorsData[0]);
+        const displayActorAge = displayActor.calcAge();
         return (   
             <div>
             <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={ActorsData[0].image} />
+                <Card.Img variant="top" src={displayActor.image} />
                 <Card.Body>
-                    <Card.Title><Card.Link href={ActorsData[0].imdbLink}>{`${ActorsData[0].firstName} ${ActorsData[0].lastName}`}</Card.Link></Card.Title>
+                    <Card.Title><Card.Link href={displayActor.imdbLink}>{`${displayActor.firstName} ${displayActor.lastName}`}</Card.Link></Card.Title>
                     <ListGroup className="list-group-flush">
-                        <ListGroupItem>Date of Birth: {ActorsData[0].birthDay}</ListGroupItem>
-                        <ListGroupItem>Age: {ActorsData[0].birthDay}</ListGroupItem>
+                        <ListGroupItem>Date of Birth: {displayActor.birthDay}</ListGroupItem>
+                        <ListGroupItem>Age: {displayActorAge}</ListGroupItem>
                     </ListGroup>
                 </Card.Body>
             </Card>
@@ -34,4 +37,3 @@ export default class ActorCard extends Component {
     }
 }
  
-// export default ActorCard;
