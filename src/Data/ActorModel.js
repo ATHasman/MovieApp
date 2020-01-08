8a55ec13669bfe0177cebbb941a2633babd90f9c
Ethan Hasman <hasman1982@gmail.com>
2020-01-08 09:40:33 +0200

    Excluding class Actor as default

--- src/Data/ActorModel.js
+++ src/Data/ActorModel.js
@@ -4,28 +4,28 @@
 //      *Object Actor[index] from ActorsData.Json
 //      ** firstName(String), lastName(String), birthDay(YYYY-MM-DD), image(URL), imdbLink(URL)
 
-export default class actor {
-    constructor (firstName, lastName, birthDay, image, imdbLink)    {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.birthDay = birthDay;
-            this.image = image;
-            this.imdbLink = imdbLink;
-    }
+// export default class actor {
+//     constructor (firstName, lastName, birthDay, image, imdbLink)    {
+//             this.firstName = firstName;
+//             this.lastName = lastName;
+//             this.birthDay = birthDay;
+//             this.image = image;
+//             this.imdbLink = imdbLink;
+//     }
 
-    // ActorMethods:
-    // calcAge() calculates Age from birthday date [yyyy-mm-dd] format and returns age(number) in years
-    calcAge()   {   
-        const dateOfBirth = new Date(this.birthDay);
-        const today = new Date();
-        let age = today.getFullYear() - dateOfBirth.getFullYear();
-        let month = today.getMonth() - dateOfBirth.getMonth();
-        if ((month < 0) || ((month === 0) && (today.getDate() < dateOfBirth.getDate()))) {
-            age--;  // accounts the difference in months and days and reduces one year 
-        }
-        return age;
-    }
-};
+//     // ActorMethods:
+//     // calcAge() calculates Age from birthday date [yyyy-mm-dd] format and returns age(number) in years
+//     calcAge()   {   
+//         const dateOfBirth = new Date(this.birthDay);
+//         const today = new Date();
+//         let age = today.getFullYear() - dateOfBirth.getFullYear();
+//         let month = today.getMonth() - dateOfBirth.getMonth();
+//         if ((month < 0) || ((month === 0) && (today.getDate() < dateOfBirth.getDate()))) {
+//             age--;  // accounts the difference in months and days and reduces one year 
+//         }
+//         return age;
+//     }
+// };
 
 export default class actorModel {
     constructor(ActorObject, lastName, birthDay, image, imdbLink) {
